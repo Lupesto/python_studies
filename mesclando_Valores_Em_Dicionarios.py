c496ecf1a3d4407d3aa402c19f5753b63283473b
Lupesto <lucasthe2@gmail.com>
2022-08-06 00:36:36 -0300

    Mesclando valores em dicionários

---
+++ mesclando_Valores_Em_Dicionarios.py
@@ -0,0 +1,14 @@
+titulos = ['moto g1', 'moto g2', 'moto g3']
+precos = [56, 45, 24.90]
+
+
+dicio = {}
+a = 0
+
+
+for i in titulos:
+    dicio[i] = precos[a]
+    a += 1
+    
+print(precos)
+print(dicio)
